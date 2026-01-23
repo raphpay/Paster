a096d6e1935075667f1cbca4ba45010a50e57fb9
Raphaël Payet <raphaelpayet@MacBook-Pro-de-Raphael.local>
2026-01-23 14:34:37 +0100

    Handle empty clipboard case #5

--- frontend/src/components/History.tsx
+++ frontend/src/components/History.tsx
@@ -22,9 +22,17 @@ function History({ history }: Props) {
   };
 
   return (
-    <div className="h-full grow flex flex-col p-2">
-      <Card item={history[0] ?? emptyItem} isSelected={false} />
-    </div>
+    <>
+      {history.length === 0 ? (
+        <div className="h-full w-full grow flex flex-col p-2">
+          <Card item={history[0] ?? emptyItem} isSelected={false} />
+        </div>
+      ) : (
+        <div className="h-full w-full grow flex items-center justify-center">
+          <h2 className="text-xl font-semibold">Pas de texte copié</h2>
+        </div>
+      )}
+    </>
   );
 }
 
